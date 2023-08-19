abe4ae8030d8657a9dd2197369b2788120ea6bb4
blakeM42 <127363518+blakeM42@users.noreply.github.com>
2023-08-19 16:06:43 -0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$message = $_POST['message'];
+	
+	//Database connection 
+	$conn = new mysqli('localhost','root','','mangolawines');
+	if($conn->connect_error){
+		die('Connection Failed : '.$conn->connect_error);
+	}else{
+		$stmt = %conn->prepare("insert into registration(name, email, phone, message)
+		values(?, ?, ? , ?)")
+		$stmt->bing_param("ssis",$name, $email, $phone, $message);
+		$stmt->execute();
+		echo "registration successfully...";
+		$stmt->close();
+		$conn->close();
+?>
